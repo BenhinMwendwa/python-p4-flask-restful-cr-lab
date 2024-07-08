b8844fe54d616ae4fab9822ddc41246a2b5b7e20
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-07-08 22:20:11 +0300

    lab

--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name=db.column(db.String)
+    price=db.column(db.Float)
+    image = db.Column(db.String)
+    
+    def __repr__(self):
+     return f'<Plant {self.name} | Price: {self.price}>'
